038ae96842dc7eef652fc3ec24c990a13b178755
dungraymond <dungraymond07@gmail.com>
2023-02-02 14:29:15 +0700

    done rewrite reverse.js

--- reverse.js
+++ reverse.js
@@ -1,13 +1,24 @@
-// import data45 from "./data45.js"
 import fs from "fs";
 
-let c = JSON.parse(fs.readFileSync('./data55.json'))
 
-let arr=[]
+let c55 = JSON.parse(fs.readFileSync('./data55.json'))
+
+let arr55=[]
 for(let [i, item] of c.entries()) {
-  arr.unshift(item)
+  arr55.unshift(item)
   // console.log(i)
 }
 
-fs.writeFileSync("./new55.json",JSON.stringify(arr))
+fs.writeFileSync("./reverb55.json",JSON.stringify(arr55))
 // console.log(arr)
+
+
+// let c45 = JSON.parse(fs.readFileSync('./data55.json'))
+
+// let arr45=[]
+// for(let [i, item] of c.entries()) {
+//   arr45.unshift(item)
+//   // console.log(i)
+// }
+
+// fs.writeFileSync("./reverb45.json",JSON.stringify(arr45))
