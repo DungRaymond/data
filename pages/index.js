6622988697160c35b2ab1ea155bb5f1b8cd43129
dungraymond <dungraymond07@gmail.com>
2023-02-28 16:25:23 +0700

    added navigation button with material design

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Link from "next/link"
 export function HomePage() {
   return(
   <>
-    <h1><Link href="/45/979-1004">Mega 45</Link></h1>
-    <h1><Link href="/55/700-800">Mega 55</Link></h1>
+    <h1><Link href="/45/979-1004">1</Link></h1>
+    <h1><Link href="/55/700-800">2</Link></h1>
   
   </>
   )
