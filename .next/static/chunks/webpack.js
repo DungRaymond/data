a2da8cdadff188c57b01b988b397dbfa3174d2f4
dungraymond <dungraymond07@gmail.com>
2023-03-17 06:53:03 +0700

    fast commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -119,7 +119,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3296345932d98253"; }
+/******/ 		__webpack_require__.h = function() { return "9a3792365594ee63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
