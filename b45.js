c58429e29df2cb34609f44bf2c85975bb3f3289a
dungraymond <dungraymond07@gmail.com>
2023-06-02 16:57:41 +0700

    fast commit

--- b45.js
+++ b45.js
@@ -5,7 +5,7 @@ const data45 = JSON.parse(fs.readFileSync('./json/reverb45.json'))
 let temp = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 fs.writeFileSync('./json/freq45.json', "{\n\t\"stat\": " + JSON.stringify(temp) + "\n}")
 
-let arr100 = data45.slice(data45.length - 16, data45.length)
+let arr100 = data45.slice(data45.length - 18, data45.length)
 arr100 = arr100.map(each => {
   return each.ketqua
 })
