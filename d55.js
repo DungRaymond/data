c3aa8da10dc9ea700778a3e6726b71f60ad1ec7e
dungraymond <dungraymond07@gmail.com>
2023-06-21 17:48:22 +0700

    fast commit

---
+++ d55.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+async function extract() {
+  for(i = 1; i < 56; i++) {
+    if(i < 10) {
+      let pivot = "0" + i;
+      
+    } else {
+      let pivot = "" + i;
+    }
+  }
+
+}
