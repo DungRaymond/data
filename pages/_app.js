fcc1316ddc1ea8778f4e9e668aed08e53ae1ae81
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2023-07-15 04:11:38 +0700

    fast commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   return(
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.75" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.7" />
       </Head>
       <Component {...pageProps} />
     </>
