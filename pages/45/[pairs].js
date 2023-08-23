61e9797d38fc29bde3410270555f28836c77af43
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2023-08-24 02:41:54 +0700

    fast commit

--- pages/45/[pairs].js
+++ pages/45/[pairs].js
@@ -9,6 +9,7 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { Button, Grid, useMediaQuery } from '@mui/material';
+import ChartistGraph from 'react-chartist';
 
 `use client`
 
