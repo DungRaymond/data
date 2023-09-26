e867d79c87778651ca3ee0031aad98726cb86adf
dungraymond <dungraymond07@gmail.com>
2023-09-26 17:22:57 +0700

    fast commit

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,11 @@ export function HomePage() {
       }}>
         Power
       </button>
+      <button type='button' className='pure-material-button-contained' onClick={() => {
+        router.push("/test/0-878-899")
+      }}>
+        Test
+      </button>
     </div>
     
     <style jsx>{
