656524e579a8cbcaef50bac66ac2b6afe2595c4b
RayAMD <dungraymond07@gmail.com>
2024-03-10 19:52:36 +0700

    fast commit

--- pages/56/[pairs].js
+++ pages/56/[pairs].js
@@ -30,7 +30,7 @@ import isInclude from '../../modules/combination.js'
 
 // PAGE COMPONENT
 
-ChartJS.defaults.font.size = 18;
+ChartJS.defaults.font.size = 17;
 ChartJS.defaults.font.weight = '500';
 
 export function Page({aData}) {
